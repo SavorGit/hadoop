6b2f39f65abcbf1708d9d3a82af03cba7eaa6c19
lz958942 <404644381@qq.com>
2017-08-29 14:49:14 +0800

    修改

--- map-reduce/hdfs/src/main/java/com/littlehotspot/hadoop/mr/hdfs/module/CleanByRegexScheduler.java
+++ map-reduce/hdfs/src/main/java/com/littlehotspot/hadoop/mr/hdfs/module/CleanByRegexScheduler.java
@@ -41,6 +41,10 @@ import java.util.regex.Pattern;
  */
 public class CleanByRegexScheduler extends Configured implements Tool {
 
+    private static final int BYTES_64M = 128 * 1024 * 1024;
+    private static final int BYTES_128M = 128 * 1024 * 1024;
+    private static final int BYTES_256M = 256 * 1024 * 1024;
+
     /**
      * 运行
      *
@@ -82,10 +86,18 @@ public class CleanByRegexScheduler extends Configured implements Tool {
             if (StringUtils.isBlank(jobName)) {
                 jobName = this.getClass().getName();
             }
-            this.getConf().setLong("mapreduce.input.fileinputformat.split.maxsize", 134217728);
+
+            // Map side tuning
+            this.setMapperConfig();
+
+            // Reduce side tuning
+            this.setReduceConfig();
+
+
+            this.getConf().setLong("mapreduce.input.fileinputformat.split.maxsize", BYTES_128M);
+            this.getConf().setPattern(CleanByRegexConstant.HadoopConfig.Key.MAPPER_INPUT_FORMAT_REGEX_PATTERN, Pattern.compile(matcherRegex));// 配置 Mapper 输入的正则匹配对象
             System.out.println(this.getConf().getLong("mapreduce.input.fileinputformat.split.maxsize", -1));
 
-            this.getConf().setPattern(CleanByRegexConstant.HadoopConfig.Key.MAPPER_INPUT_FORMAT_REGEX_PATTERN, Pattern.compile(matcherRegex));// 配置 Mapper 输入的正则匹配对象
             Job job = Job.getInstance(this.getConf(), jobName);
             job.setJarByClass(this.getClass());
 
@@ -123,5 +135,51 @@ public class CleanByRegexScheduler extends Configured implements Tool {
             e.printStackTrace();
             return 1;
         }
+    }
+
+    // Reduce side tuning
+    private void setReduceConfig() {
+        // Reduce Shuffle
+        this.getConf().setInt("mapred.reduce.parallel.copies", 10);
+        this.getConf().setInt("mapred.reduce.copy.backoff", 300);
+
+        // Reduce Merge
+        this.getConf().setDouble("mapred.job.shuffle.input.buffer.percent", 0.7);
+        this.getConf().setDouble("mapred.job.shuffle.merge.percent", 0.66);
+        this.getConf().set("mapred.child.Java.opts", "-Xmx1024m");
+
+        // Reduce Reduce
+        this.getConf().setDouble("mapred.job.reduce.input.buffer.percent", 0.0);
+
+        this.getConf().setLong("mapred.reduce.tasks", 1);
+        this.getConf().setLong("mapred.tasktracker.reduce.tasks.maximum", 2);
+        this.getConf().setDouble("mapred.reduce.slowstart.completed.maps", 0.05);
+    }
+
+    // Map side tuning
+    private void setMapperConfig() {
+        // Mapper InputFormat
+        this.getConf().setInt("mapred.min.split.size", BYTES_64M);
+        this.getConf().setInt("mapred.max.split.size", BYTES_128M);
+//            this.getConf().setLong("dfs.block.size", BYTES_128M);
+
+        // Mapper Buffer
+        this.getConf().setInt("io.sort.mb", BYTES_256M);
+//            this.getConf().setLong("mapreduce.task.io.sort.mb", BYTES_256M);
+        this.getConf().setDouble("io.sort.spill.percent", 0.8);
+
+        // Mapper Merge
+        this.getConf().setLong("io.sort.factor", 64);
+//            this.getConf().setLong("mapreduce.task.io.sort.factor", 64);
+
+        // Mapper Combine
+        this.getConf().setLong("min.num.spill.for.combine", 3);
+
+        // Mapper 输出中间数据到磁盘
+        this.getConf().setBoolean("mapred.compress.map.output", true);
+        this.getConf().set("mapred.map.output.compression.codec", "GzipCodec");
+
+
+        this.getConf().setLong("mapred.tasktracker.map.tasks.maximum", 2);
     }
 }
