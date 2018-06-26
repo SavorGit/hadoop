4eaf708d31009d5680c10887ae8c7cdfc48ad4ef
lz958942 <404644381@qq.com>
2018-06-26 16:23:12 +0800

    修改

--- map-reduce/export/excel/jdbc-excel/src/main/java/com/littlehotspot/hadoop/mr/export/excel/hive/ExportExcelFromJDBCScheduler.java
+++ map-reduce/export/excel/jdbc-excel/src/main/java/com/littlehotspot/hadoop/mr/export/excel/hive/ExportExcelFromJDBCScheduler.java
@@ -46,102 +46,107 @@ public class ExportExcelFromJDBCScheduler extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        System.out.println();
-        System.out.println("Jar action configuration");
-        System.out.println("=================================================================");
+        try {
+            System.out.println();
+            System.out.println("Jar action configuration");
+            System.out.println("=================================================================");
 
-        MapReduceConstant.CommonVariables.initMapReduce(this.getConf(), args);// 解析参数并初始化 MAP REDUCE
+            MapReduceConstant.CommonVariables.initMapReduce(this.getConf(), args);// 解析参数并初始化 MAP REDUCE
 
-        // Mapper 输入正则表达式
-        String jobName = ArgumentFactory.getParameterValue(Argument.JobName);
-        ArgumentFactory.printInputArgument(Argument.JobName, jobName, false);
+            // Mapper 输入正则表达式
+            String jobName = ArgumentFactory.getParameterValue(Argument.JobName);
+            ArgumentFactory.printInputArgument(Argument.JobName, jobName, false);
 
-        // JDBC 驱动器
-        String jdbcDriver = ArgumentFactory.getParameterValue(Argument.JDBCDriver);
-        ArgumentFactory.printInputArgument(Argument.JDBCDriver, jdbcDriver, false);
+            // JDBC 驱动器
+            String jdbcDriver = ArgumentFactory.getParameterValue(Argument.JDBCDriver);
+            ArgumentFactory.printInputArgument(Argument.JDBCDriver, jdbcDriver, false);
 
-        // JDBC 连接字符串
-        String jdbcUrl = ArgumentFactory.getParameterValue(Argument.JDBCUrl);
-        ArgumentFactory.printInputArgument(Argument.JDBCUrl, jdbcUrl, false);
+            // JDBC 连接字符串
+            String jdbcUrl = ArgumentFactory.getParameterValue(Argument.JDBCUrl);
+            ArgumentFactory.printInputArgument(Argument.JDBCUrl, jdbcUrl, false);
 
-        // JDBC 用户
-        String jdbcUser = ArgumentFactory.getParameterValue(Argument.JDBCUsername);
-        ArgumentFactory.printInputArgument(Argument.JDBCUsername, jdbcUser, false);
+            // JDBC 用户
+            String jdbcUser = ArgumentFactory.getParameterValue(Argument.JDBCUsername);
+            ArgumentFactory.printInputArgument(Argument.JDBCUsername, jdbcUser, false);
 
-        // JDBC 密码
-        String jdbcPass = ArgumentFactory.getParameterValue(Argument.JDBCPassword);
-        ArgumentFactory.printInputArgument(Argument.JDBCPassword, jdbcPass, true);
+            // JDBC 密码
+            String jdbcPass = ArgumentFactory.getParameterValue(Argument.JDBCPassword);
+            ArgumentFactory.printInputArgument(Argument.JDBCPassword, jdbcPass, true);
 
-        // Workbook 路径
-        String workbook = ArgumentFactory.getParameterValue(Argument.Workbook);
-        ArgumentFactory.printInputArgument(Argument.Workbook, workbook, false);
+            // Workbook 路径
+            String workbook = ArgumentFactory.getParameterValue(Argument.Workbook);
+            ArgumentFactory.printInputArgument(Argument.Workbook, workbook, false);
 
-        // Sheet配置: sheet索引,hdfs目录名,sheet名称,数据中字段的分割符
-        String sheetName = ArgumentFactory.getParameterValue(Argument.Sheet);
-        ArgumentFactory.printInputArgument(Argument.Sheet, sheetName, false);
+            // Sheet配置: sheet索引,hdfs目录名,sheet名称,数据中字段的分割符
+            String sheetName = ArgumentFactory.getParameterValue(Argument.Sheet);
+            ArgumentFactory.printInputArgument(Argument.Sheet, sheetName, false);
 
-        // Title配置: sheetIndex,标题分割符,字段名1|字段名2|...
-        String sheetTitles = ArgumentFactory.getParameterValue(Argument.Title);
-        ArgumentFactory.printInputArgument(Argument.Title, sheetTitles, false);
+            // Title配置: sheetIndex,标题分割符,字段名1|字段名2|...
+            String sheetTitles = ArgumentFactory.getParameterValue(Argument.Title);
+            ArgumentFactory.printInputArgument(Argument.Title, sheetTitles, false);
 
-        // 要执行的 SQL 语句
-        String executeSQL = ArgumentFactory.getParameterValue(Argument.JDBCSql);
-        ArgumentFactory.printInputArgument(Argument.JDBCSql, executeSQL, false);
+            // 要执行的 SQL 语句
+            String executeSQL = ArgumentFactory.getParameterValue(Argument.JDBCSql);
+            ArgumentFactory.printInputArgument(Argument.JDBCSql, executeSQL, false);
 
-        // 输入 Reduce 时，Value 的正则表达式
-        String reduceInPatternValue = ArgumentFactory.getParameterValue(ExcelArgument.ReduceInPatternValue);
-        ArgumentFactory.printInputArgument(ExcelArgument.ReduceInPatternValue, reduceInPatternValue, false);
+            // 输入 Reduce 时，Value 的正则表达式
+            String reduceInPatternValue = ArgumentFactory.getParameterValue(ExcelArgument.ReduceInPatternValue);
+            ArgumentFactory.printInputArgument(ExcelArgument.ReduceInPatternValue, reduceInPatternValue, false);
 
 
-        // 准备工作
-        ArgumentFactory.checkNullValueForArgument(Argument.JDBCDriver, jdbcUrl);
-        ArgumentFactory.checkNullValueForArgument(Argument.JDBCUrl, jdbcUrl);
-        ArgumentFactory.checkNullValueForArgument(Argument.Workbook, workbook);
-        ArgumentFactory.checkNullValueForArgument(Argument.Sheet, sheetName);
-        ArgumentFactory.checkNullValueForArgument(Argument.JDBCSql, executeSQL);
-        ArgumentFactory.checkNullValueForArgument(ExcelArgument.ReduceInPatternValue, reduceInPatternValue);
-        if (StringUtils.isBlank(jobName)) {
-            jobName = this.getClass().getName();
+            // 准备工作
+            ArgumentFactory.checkNullValueForArgument(Argument.JDBCDriver, jdbcUrl);
+            ArgumentFactory.checkNullValueForArgument(Argument.JDBCUrl, jdbcUrl);
+            ArgumentFactory.checkNullValueForArgument(Argument.Workbook, workbook);
+            ArgumentFactory.checkNullValueForArgument(Argument.Sheet, sheetName);
+            ArgumentFactory.checkNullValueForArgument(Argument.JDBCSql, executeSQL);
+            ArgumentFactory.checkNullValueForArgument(ExcelArgument.ReduceInPatternValue, reduceInPatternValue);
+            if (StringUtils.isBlank(jobName)) {
+                jobName = this.getClass().getName();
+            }
+
+            String[] sheetTitleArray = null;
+            if (sheetTitles != null) {
+                sheetTitleArray = sheetTitles.split("\\|");
+            }
+
+            Pattern valuePattern = Pattern.compile(reduceInPatternValue);
+
+            System.out.println("=================================================================");
+            System.out.println();
+            System.out.println(">>> Invoking Export job task now >>>");
+            System.out.println();
+            System.out.flush();
+
+            Path outputPath = new Path(workbook);
+
+            DBConfiguration.configureDB(this.getConf(), jdbcDriver, jdbcUrl, jdbcUser, jdbcPass);
+
+            Job job = Job.getInstance(this.getConf(), jobName);
+            job.setJarByClass(this.getClass());
+
+            job.setMapperClass(HiveInputMapper.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(Text.class);
+
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            job.setOutputFormatClass(ExcelFileOutputFormat.class);
+
+            // 设置输入输出
+            HiveInputFormat.setInput(job, SimpleDataWritable.class, executeSQL, this.getCountQuery(executeSQL));
+            ExcelFileOutputFormat.setOutputPath(job, outputPath, sheetName, sheetTitleArray, valuePattern);
+
+            boolean status = job.waitForCompletion(true);
+            if (!status) {
+                throw new Exception("MapReduce task execute failed.........");
+            }
+
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
         }
-
-        String[] sheetTitleArray = null;
-        if (sheetTitles != null) {
-            sheetTitleArray = sheetTitles.split("\\|");
-        }
-
-        Pattern valuePattern = Pattern.compile(reduceInPatternValue);
-
-        System.out.println("=================================================================");
-        System.out.println();
-        System.out.println(">>> Invoking Export job task now >>>");
-        System.out.println();
-        System.out.flush();
-
-        Path outputPath = new Path(workbook);
-
-        DBConfiguration.configureDB(this.getConf(), jdbcDriver, jdbcUrl, jdbcUser, jdbcPass);
-
-        Job job = Job.getInstance(this.getConf(), jobName);
-        job.setJarByClass(this.getClass());
-
-        job.setMapperClass(HiveInputMapper.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.setOutputFormatClass(ExcelFileOutputFormat.class);
-
-        // 设置输入输出
-        HiveInputFormat.setInput(job, SimpleDataWritable.class, executeSQL, this.getCountQuery(executeSQL));
-        ExcelFileOutputFormat.setOutputPath(job, outputPath, sheetName, sheetTitleArray, valuePattern);
-
-        boolean status = job.waitForCompletion(true);
-        if (!status) {
-            throw new Exception("MapReduce task execute failed.........");
-        }
-
-        return 0;
     }
 
     // 生成获取总记录数据的 SQL 语句
